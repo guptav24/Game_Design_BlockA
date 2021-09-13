1e1bc1938e2d0567e6ea91b2b63ebba0b9ffb78d
guptav24 <vivaanjg@gmail.com>
2021-09-13 09:59:50 -0500

    09/13/2021

--- Challenge2_DataTypes.py
+++ Challenge2_DataTypes.py
@@ -4,3 +4,20 @@ import os
 os.system('cls')
 
 #This is about data types and how strings work
+userInput = input("Type something: ")
+try:    #Trial and error function
+    int(userInput)
+    check = True
+except ValueError:
+    check = False
+
+if (check):
+    print()
+else:
+    print(len(userInput))   #Prints the length of the input in characters
+
+for i in userInput:
+    print (i)
+
+if len(userInput > 3):
+    print (userInput[3])
