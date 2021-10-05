dc4e74d65178f809ca9adcc0f236d6d387bb46d7
guptav24 <vivaanjg@gmail.com>
2021-10-05 09:43:03 -0500

    10/05/2021

--- Hangman_game.py
+++ Hangman_game.py
@@ -14,16 +14,18 @@ def updateWord(randWord,guesses):
             print("_",end=" ")
 
 def menu():
-    print("This is a guessing game! Choose a category!")
-    print("######################################")
-    print("#                MENU                #")
-    print("#                                    #")
-    print("#             1. ANIMALS             #")
-    print("#             2. FRUITS              #")
-    print("#             3. COMP PARTS          #")
-    print("#             4. EXIT                #")
-    print("# To play the game, select 1-3, to exit, select 4.")
-    print("######################################")
+    print("####################################################")
+    print("#    This is a guessing game! Choose a category!   #")
+    print("#                                                  #")
+    print("#                       MENU                       #")
+    print("#                                                  #")
+    print("#                   1. ANIMALS                     #")
+    print("#                   2. FRUITS                      #")
+    print("#                   3. COMP PARTS                  #")
+    print("#                   4. EXIT                        #")
+    print("#                                                  #")
+    print("# To play the game, select 1-3, to exit, select 4. #")
+    print("####################################################")
     print()
     sel=input("What would you like to play? ")
     try:
@@ -51,31 +53,37 @@ name = input("What is your name? ")
 counter = 0
 wins = 0
 sel = menu()
+game = "y"
 
-while sel!=4:
+while sel!=4 and ("Y" and "y" in game):
     print("Good Luck "+name+"! You have 5 lives")
     turns = 5
     counter += 1
     randWord = selWord(sel)
     randWord = randWord.lower()
     wordCount = len(randWord)
-    letCount = 0
     print(randWord)
-    guesses = " "
+    guesses = ""
     updateWord(randWord,guesses)
+    letCount = 0
 
     while turns > 0 and letCount<wordCount:
+        letCount = 0
         print()
         newGuess = input("Guess a letter: ")
         newguess = newGuess.lower()
         if newGuess in randWord:
             guesses+=newGuess
-            letCount+=1
             print("You guessed a correct letter!")
         else:
             turns -= 1
             print("That is not in the word. You have ", turns, " lives left")
-        updateWord(randWord,guesses)
+        for letter in randWord:
+            if letter in guesses:
+                print (letter,end=" ")
+                letCount+=1
+            else:
+                print("_",end=" ")
 
     if turns == 0:
         print("You lose!")
@@ -84,7 +92,9 @@ while sel!=4:
         print("You win!")            
     game = input("Do you want to play again? Type Y for yes or N for no: ")
     if ("Y" and "y" in game):
-        menu()
+        sel = menu()
+    if ("n" and "N" in game):
+        sel = 4
 
 print("Thank you for playing!")
         
