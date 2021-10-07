a56ba7d766eb0d2b64817cd7ae3714f3760940ee
guptav24 <vivaanjg@gmail.com>
2021-10-07 09:24:57 -0500

    10/07/2021

--- TestRandom.py
+++ TestRandom.py
@@ -13,9 +13,6 @@ def updateWord(randWord,guesses):
         else:
             print("_",end=" ")
 
-counter = 0
-wins = 0
-
 def menu():
     print("####################################################")
     print("#    This is a guessing game! Choose a category!   #")
@@ -29,8 +26,6 @@ def menu():
     print("#                                                  #")
     print("# To play the game, select 1-3, to exit, select 4. #")
     print("#                                                  #")
-    print("#                 Games played: " , counter, "                #")
-    print("#                  Games won: " , wins, "                  #")
     print("####################################################")
     print()
     sel=input("What would you like to play? ")
@@ -40,8 +35,10 @@ def menu():
             check = True
             return sel
     except ValueError:
-        print("Give me a number from 1 - 4")
+        print("Give me a number from 1 - 4. Try Again")
+        sel=input("What would you like to play? ")
         check = False
+        
 def selWord(sel):
     if sel == 1:
         randWord = random.choice(animals)
@@ -51,22 +48,21 @@ def selWord(sel):
         randWord = random.choice(compParts)
     return randWord
 
-animals = ["tiger","elephant","monkey","lion"]
-compParts = ["keyboard","monitor","computer","case","trackpad"]
+animals = ["tiger","elephant","monkey","lion","zebra","panther","rhino","dog","cat","bird","fish"]
+compParts = ["keyboard","monitor","computer","case","trackpad","headphone","motherboard","microphone"]
 fruits = ["peach","apple","orange","grape","cherry","watermelon","banana","strawberry","blueberry","mango"]
 
 name = input("What is your name? ")
-counter = 0
-wins = 0
+maxScore = 0
 sel = menu()
 game = "y"
 while sel!=4 and ("Y" and "y" in game):
-    print("Good Luck "+name+"! You have 5 lives")
-    turns = 5
     randWord = selWord(sel)
     randWord = randWord.lower()
     wordCount = len(randWord)
-    print(randWord)
+    turns = len(randWord) + 3
+    print("Good Luck ",name,"! You have ", turns," lives")
+    print (randWord)
     guesses = ""
     updateWord(randWord,guesses)
     letCount = 0
@@ -91,12 +87,14 @@ while sel!=4 and ("Y" and "y" in game):
 
     if turns == 0:
         print("You lose!")
-        counter += 1
     else:
         print()
         print("You win!")
-        counter += 1
-        wins+=1
+    os.system('cls')
+    score = 3*wordCount+5*turns
+    if score>maxScore:
+        maxScore = score
+    print (maxScore)
     game = input("Do you want to play again? Type Y for yes or N for no: ")
     if ("Y" and "y" in game):
         sel = menu()
